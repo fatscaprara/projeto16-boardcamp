f84ac06e22607aef6a3c999de463fe8b9950226a
Fabio <fats140701@gmail.com>
2023-05-13 10:38:32 -0400

    inicial config

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const server = express();
+server.use(express.json());
+server.use(cors());
+
+const port = process.env.PORT || 5000;
+server.listen(5000, () => console.log(`Server running in port: ${port}`));
